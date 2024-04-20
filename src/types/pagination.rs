216bc5569aec2a970bcff332bad25f0c5b8d03d5
Bilal Abu Saad <bilalabusaad@gmail.com>
2024-04-20 12:55:39 -0400

    add docs to pagination.rs
    it looks nice cargo doc --open

--- src/types/pagination.rs
+++ src/types/pagination.rs
@@ -1,14 +1,29 @@
+use handle_errors::Error;
 use std::collections::HashMap;
 
-use handle_errors::Error;
-
-/// Start and end cursors of questions to return.
+/// Pagination struct that is getting extracted from query params.
 #[derive(Debug)]
 pub struct Pagination {
+    /// The index of the first item to be retuned.
     pub start: usize,
+    /// The index of the last item to be retuned.
     pub end: usize,
 }
 
+/// Extract Pagination params from the given query params.
+/// # Example query
+/// GET `/questions?start=1&end=10` returns a Pagination { 1, 10 }.
+/// # Example usage
+/// ```rust
+///  let query = HashMap::from(
+///     [("start".to_string(), "1".to_string()),
+///     ("end".to_string(), "10".to_string())
+///  ];
+///
+///  let p = types::pagination::extract_pagination(query).unwrap();
+///  assert_eq!(p.start, 1);
+///  assert_eq!(p.end, 10);
+/// ```
 pub fn extract_pagination(params: HashMap<String, String>) -> Result<Pagination, Error> {
     if params.contains_key("start") && params.contains_key("end") {
         return Ok(Pagination {
