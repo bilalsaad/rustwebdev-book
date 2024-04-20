319498ffb36c26e50c9d7c67b2ac89caaba0be96
Bilal Abu Saad <bilalabusaad@gmail.com>
2024-04-20 11:48:11 -0400

    move errors into its own file

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,8 @@ use std::collections::HashMap;
 use std::sync::Arc;
 use tokio::sync::RwLock;
 
+mod error;
+
 use warp::{
     http::{Method, StatusCode},
     Filter,
@@ -39,62 +41,6 @@ struct Question {
 
 #[derive(Serialize, Deserialize, Debug, Clone, PartialEq, Eq, Hash)]
 struct QuestionId(String);
-
-mod error {
-
-    use warp::{
-        filters::{body::BodyDeserializeError, cors::CorsForbidden},
-        http::StatusCode,
-        reject::Reject,
-        Rejection, Reply,
-    };
-
-    #[derive(Debug)]
-    pub enum Error {
-        ParseError(std::num::ParseIntError),
-        MissingParameters,
-        QuestionNotFound,
-    }
-
-    impl std::fmt::Display for Error {
-        fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-            match *self {
-                Error::ParseError(ref err) => {
-                    write!(f, "Cannot parse parameter: {}", err)
-                }
-                Error::MissingParameters => write!(f, "Missing parameter"),
-                Error::QuestionNotFound => write!(f, "Question not found"),
-            }
-        }
-    }
-
-    impl Reject for Error {}
-
-    pub async fn return_error(r: Rejection) -> Result<impl Reply, Rejection> {
-        println!("{:?}", r);
-        if let Some(error) = r.find::<Error>() {
-            Ok(warp::reply::with_status(
-                error.to_string(),
-                StatusCode::RANGE_NOT_SATISFIABLE,
-            ))
-        } else if let Some(error) = r.find::<CorsForbidden>() {
-            Ok(warp::reply::with_status(
-                error.to_string(),
-                StatusCode::FORBIDDEN,
-            ))
-        } else if let Some(error) = r.find::<BodyDeserializeError>() {
-            Ok(warp::reply::with_status(
-                error.to_string(),
-                StatusCode::UNPROCESSABLE_ENTITY,
-            ))
-        } else {
-            Ok(warp::reply::with_status(
-                "Route not found".to_string(),
-                StatusCode::NOT_FOUND,
-            ))
-        }
-    }
-}
 
 /// Start and end cursors of questions to return.
 #[derive(Debug)]
